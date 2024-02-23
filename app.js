5fcf8b86dc6eb5f4745d32ffd52fe3c00739c4e5
Evanilson Pereira <pevanilson99@gmail.com>
2024-02-23 11:21:02 -0300

    Conexão ao banco de dados

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = 5000 || process.env.PORT;
-// const db = require('./database/db.js');
-// db();
+const db = require('./database/db.js');
+db();
 
 
 app.get('/', (req, res) => {
