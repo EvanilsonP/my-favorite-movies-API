a33f0f606531963d2d7648b4d3d64291e59b96ed
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-15 17:20:47 -0300

    Using Jest

---
+++ README.md
@@ -0,0 +1,8 @@
+# Sobre
+CRUD - API contendo a lista dos meus filmes favoritos.
+
+# Como usar
+Clone esse repositório git@github.com:EvanilsonP/my_favorite_movies_API.git;  
+Rode o comando npm install no terminal para instalar as dependências;  
+Crie um arquivo .env e siga as instruções do arquivo .env.example; 
+ 
--- movieController.test.js
+++ movieController.test.js
@@ -1,4 +1,4 @@
-const { homePage, createMovie } = require('./controllers/MovieController');
+const { homePage, createMovie, allMovies } = require('./controllers/MovieController');
 
 describe('homePage function', () => {
 
@@ -38,5 +38,31 @@ describe('homePage function', () => {
         }))
     });
 
+    test('should return status 201 and the created movie when all fields are provided', async () => {
+        const req = {
+            body: {
+                title: 'Test Movie',
+                description: 'This is a test movie',
+                image_url: 'test-image.jpg',
+                trailer_url: 'test-trailer.mp4'
+            }
+        };
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+        };
+
+        await createMovie(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(201);
+        expect(res.json).toHaveBeenCalledWith(expect.objectContaining({
+            title: 'Test Movie',
+            description: 'This is a test movie',
+            image_url: 'test-image.jpg',
+            trailer_url: 'test-trailer.mp4'
+        }));
+    });
 });
+
+
 
